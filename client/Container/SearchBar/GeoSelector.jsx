0f23923db743979515487365ff7aa50c3def72d6
Andrew Sutedja <andrew.sutedja@gmail.com>
2016-09-16 23:53:08 -0700

    Implement zoom to city by navbar

--- client/Container/SearchBar/GeoSelector.jsx
+++ client/Container/SearchBar/GeoSelector.jsx
@@ -3,26 +3,24 @@ import CityOption from './CityOption.jsx';
 
 class GeoSelector extends React.Component {
   constructor (props) {
-    super (props);
-    
-    this.state = {
-      value: 'select'
-    };
+    super(props);
   }
 
-  handleSelection (event) {
-    this.setState({value: event.target.value});
-    this.props.handleCitySearch(event);
-    console.log('selection changed!... and is now', this.state.value);
+  change() {
+    var selectBox = document.getElementById("selectBox");
+    var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+    console.log(selectedValue,  'value maybe');
+    this.props.city(selectedValue);
+    this.props.change(selectedValue);
   }
 
   render () {
     let cities = this.props.cities;
     return (
       <div className="GeoSelectContainer"> 
-          <select value={this.state.value} onChange={this.handleSelection.bind(this)}>
+          <select id='selectBox' onChange={this.change.bind(this)}>
             {cities.map((city) => 
-               <option id={city} value={city}>{city}</option>               
+               <CityOption city={city} />
             )}
           </select>
       </div>
